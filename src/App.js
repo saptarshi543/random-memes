abb8c0340eab6e2e536cc5e60e8e62c92e3449aa
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2023-01-26 11:38:03 +0530

    component can now be reloaded at a certain time interval

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Show from './Show.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>test</p>
+<Show />
+
       </header>
     </div>
   );
---
+++ src/Show.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+function sleep(time){
+	 return new Promise(resolve => setTimeout(resolve, time))
+}
+
+function Driver(){
+	const [refresh, setRefresh]=React.useState(0);
+for(let i=0;i<10;i++){
+
+// sleep(5000).then(()=>{
+//    console.log(i);
+//    setRefresh(Math.random())
+// })
+
+}
+
+}
+
+function Show(){
+
+	return(
+<div>
+		<p>Show</p>
+		<Driver />
+</div>
+		);
+}
+export default Show;
